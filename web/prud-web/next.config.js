7743ec42f9f39b42ac7030e09e51c72fce5b7eb2
nighmared <joniii@ethz.wtf>
2023-07-14 23:49:05 +0700

    commented out rewrite

--- web/prud-web/next.config.js
+++ web/prud-web/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: `http://${process.env.WEB_API_HOST}:${process.env.WEB_API_PORT}/api/:path*`
-            }
-        ]
-    },
+    // async rewrites() {
+    //     return [
+    //         {
+    //             source: '/api/:path*',
+    //             destination: `http://${process.env.WEB_API_HOST}:${process.env.WEB_API_PORT}/api/:path*`
+    //         }
+    //     ]
+    // },
     output: 'standalone',
 }
 
