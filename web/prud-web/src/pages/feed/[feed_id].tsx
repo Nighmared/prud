cf85e58557cf957e0f21be528ab09297f530b809
nighmared <joniii@ethz.wtf>
2023-07-15 00:03:41 +0700

    please

--- web/prud-web/src/pages/feed/[feed_id].tsx
+++ web/prud-web/src/pages/feed/[feed_id].tsx
@@ -12,7 +12,6 @@ const App = () => {
     const [postsResponse, setPostsResponse] = useState<ReadPostsResponse>();
     useEffect(() => {
         if (!router.isReady) return;
-        console.log(router.query.feed_id)
         const feedId = Number.parseInt(router.query.feed_id as string);
         fetchReadPosts(feedId, setPostsResponse)
     }, [router.isReady, router.query.feed_id])
