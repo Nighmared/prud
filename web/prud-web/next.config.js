bbe773834150c986308bfc6299b60ffbfa67451c
nighmared <joniii@ethz.wtf>
2023-07-15 17:13:11 +0700

    updated rewrites

--- web/prud-web/next.config.js
+++ web/prud-web/next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // async rewrites() {
-    //     return [
-    //         {
-    //             source: '/api/:path*',
-    //             destination: `http://${process.env.WEB_API_HOST}:${process.env.WEB_API_PORT}/api/:path*`
-    //         }
-    //     ]
-    // },
+    async rewrites() {
+        if (process.env.NODE_ENV === "development") {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: `http://localhost:8000/api/:path*`
+                }
+            ]
+        }
+    }
+    ,
+
     output: 'standalone',
 }
 
---
+++ web/prud-web/start-frontend.sh
@@ -0,0 +1,1 @@
+WEB_API_HOST=localhost WEB_API_PORT=8000 npm run dev
