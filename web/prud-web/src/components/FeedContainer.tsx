2b05bbba75ef842eb24e75624a04a4ed9923edb5
nighmared <jonas.konrath@icloud.com>
2024-01-23 15:48:47 +0100

    added noopener, noreferrer to external links

--- web/prud-web/src/components/FeedContainer.tsx
+++ web/prud-web/src/components/FeedContainer.tsx
@@ -1,46 +1,61 @@
-import { Feed } from "@/util/prud";
-import { CancelOutlined, CheckCircleOutline, LanguageOutlined, RssFeedOutlined } from "@mui/icons-material";
-import { Divider, ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip } from "@mui/material";
+import {
+  CancelOutlined,
+  CheckCircleOutline,
+  LanguageOutlined,
+  RssFeedOutlined,
+} from "@mui/icons-material";
+import {
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Tooltip,
+} from "@mui/material";
 
+import { Feed } from "@/util/prud";
 import Link from "next/link";
 
-
 interface props {
-    feed: Feed;
+  feed: Feed;
 }
 
-const FeedContainer: React.FC<props> = (({ feed }) => {
+const FeedContainer: React.FC<props> = ({ feed }) => {
+  return (
+    <>
+      <ListItem>
+        <Tooltip title={feed.enabled ? "Enabled" : "Disabled"}>
+          <ListItemIcon
+            className={feed.enabled ? "text-green-400" : "text-red-500"}
+          >
+            {feed.enabled ? <CheckCircleOutline /> : <CancelOutlined />}
+          </ListItemIcon>
+        </Tooltip>
+        <Link href={`/feed/${feed.id}`}>
+          <ListItemButton>
+            <ListItemText>{feed.title}</ListItemText>
+          </ListItemButton>
+        </Link>
+        {/* Find a better way for alignment  */}
+        <ListItemText />
+        <Tooltip title="Link to RSS Feed">
+          <ListItemIcon>
+            <a target="_blank" href={feed.feed} rel="noopener noreferrer">
+              <RssFeedOutlined className="text-orange-500" />
+            </a>
+          </ListItemIcon>
+        </Tooltip>
+        <Tooltip title="Link to Blog Homepage">
+          <ListItemIcon>
+            <a target="_blank" href={feed.url} rel="noopener noreferrer">
+              <LanguageOutlined className="text-blue-500" />
+            </a>
+          </ListItemIcon>
+        </Tooltip>
+      </ListItem>
+      <Divider />
+    </>
+  );
+};
 
-
-    return (
-        <>
-            <ListItem>
-                <Tooltip title={feed.enabled ? "Enabled" : "Disabled"}>
-                    <ListItemIcon className={feed.enabled ? "text-green-400" : "text-red-500"}>
-                        {feed.enabled ? <CheckCircleOutline /> : <CancelOutlined />}
-                    </ListItemIcon>
-                </Tooltip>
-                <Link href={`/feed/${feed.id}`}>
-                    <ListItemButton>
-                        <ListItemText>{feed.title}</ListItemText>
-                    </ListItemButton >
-                </Link>
-                {/* Find a better way for alignment  */}
-                <ListItemText />
-                <Tooltip title="Link to RSS Feed">
-                    <ListItemIcon>
-                        <a target="_blank" href={feed.feed}><RssFeedOutlined className="text-orange-500" /></a>
-                    </ListItemIcon>
-                </Tooltip>
-                <Tooltip title="Link to Blog Homepage">
-                    <ListItemIcon>
-                        <a target="_blank" href={feed.url}><LanguageOutlined className="text-blue-500" /></a>
-                    </ListItemIcon>
-                </Tooltip>
-            </ListItem>
-            <Divider />
-        </>
-    )
-})
-
-export default FeedContainer
+export default FeedContainer;
--- web/prud-web/src/components/PostContainer.tsx
+++ web/prud-web/src/components/PostContainer.tsx
@@ -1,29 +1,43 @@
-import { Post } from "@/util/prud";
 import { Divider, ListItem, ListItemText, Typography } from "@mui/material";
 
+import { Post } from "@/util/prud";
+
 interface Props {
-    post: Post;
+  post: Post;
 }
 
 const PostContainer: React.FC<Props> = ({ post }) => {
-    const published_txt = new Intl.DateTimeFormat("en-US", { year: "numeric", month: "short", day: "numeric", hour: "2-digit", minute: "2-digit" }).format(post.published * 1000)
-    const linkWithHttpEnsured = post.link.startsWith("http") ? post.link : `https://${post.link}`
-    return (
-        <>
-            <ListItem alignItems="center">
-                <ListItemText>
-                    <a className="hover:underline" href={linkWithHttpEnsured} target="_blank">
-                        <Typography variant="h4">{post.title}</Typography>
-                        <Typography variant="subtitle1">{published_txt}</Typography>
-                        <Typography variant="body1">{post.summary.substring(0, 1000)}</Typography>
-                    </a>
+  const published_txt = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(post.published * 1000);
+  const linkWithHttpEnsured = post.link.startsWith("http")
+    ? post.link
+    : `https://${post.link}`;
+  return (
+    <>
+      <ListItem alignItems="center">
+        <ListItemText>
+          <a
+            className="hover:underline"
+            href={linkWithHttpEnsured}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <Typography variant="h4">{post.title}</Typography>
+            <Typography variant="subtitle1">{published_txt}</Typography>
+            <Typography variant="body1">
+              {post.summary.substring(0, 1000)}
+            </Typography>
+          </a>
+        </ListItemText>
+      </ListItem>
+      <Divider />
+    </>
+  );
+};
 
-                </ListItemText>
-            </ListItem>
-            <Divider />
-        </>
-    )
-}
-
-
-export default PostContainer
+export default PostContainer;
